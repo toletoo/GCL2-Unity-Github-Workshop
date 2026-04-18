031cb62e53017e48d6cf99b3d1cd4c98e21eb400
Jonathan <47917658+merctraider@users.noreply.github.com>
2026-04-18 16:54:11 +0800

    Add GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("Game Settings")]
+    public int startingScore = 0;   // <-- Activity 2 script conflict target
+    public int lives = 3;           // <-- Activity 3 merge conflict target
+    public float gameSpeed = 1.0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore(int points)
+    {
+        startingScore += points;
+        Debug.Log("Score: " + startingScore);
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        Debug.Log("Lives remaining: " + lives);  // <-- Activity 3 conflict target
+
+        if (lives <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
