627cf2193c301aa30956af7c21e79d40bc5fdac7
toletoo <yomamanbruh@gmail.com>
2026-04-29 10:04:07 +0800

    Jarel | Add sprite, script and GameObject

---
+++ Assets/Scripts/JarelController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JarelController : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("I love gooning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
